f8fd58b49fa01910b780e821c583f552eafd4d9e
MarioRaafat <mariorafat10@gmail.com>
2024-06-18 00:24:44 -0700

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-""" Python script to export data in the CSV format."""
-
-import sys
+"""Exports to-do list information for a given employee ID to CSV format."""
+import csv
 import requests
+import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
-    user_data = requests.get(url + "users/{}/".format(user_id)).json()
-    user_username = user_data.get("username")
-    tasks = requests.get(url + "todos/", params={"userId": user_id}).json()
-    for task in tasks:
-        print("\"{}\", \"{}\", \"{}\", \"{}\"".format(user_id, user_username, task.get("completed"), task.get("title")))
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
